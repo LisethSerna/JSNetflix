fb1750414d6d3a21da583d13bc541ce4f2a1e39d
LisethSerna <147016146+LisethSerna@users.noreply.github.com>
2023-10-04 22:10:19 -0500

    Add files via upload

---
+++ Netflix.js
@@ -0,0 +1,43 @@
+const urls = [
+    'https://api.themoviedb.org/3/movie/popular?api_key=7be72508776961f3948639fbd796bccd&page',
+    'https://api.themoviedb.org/3/movie/popular?api_key=7be72508776961f3948639fbd796bccd&page',
+    'https://api.themoviedb.org/3/movie/popular?api_key=7be72508776961f3948639fbd796bccd&page'
+];
+window.addEventListener('DOMContentLoaded',()=>{
+    const peticiones = urls.map(peticion=>fetch(peticion));
+    Promise.all(peticiones).then(values=>{
+        return Promise.all(values.map(r=>r.json()))
+    }).then(catologos=>{
+        const [catalogoUno,catalogoDos,catalogoTres] = catologos;
+        // Catalogo uno
+        const populares = document.getElementById('populares');
+        catalogoUno.results.forEach(pelicula => {
+            const article = document.createElement('article');
+            article.classList.add('pelicula');
+            const img = document.createElement('img');
+            img.src = 'https://image.tmdb.org/t/p/original/'+pelicula.poster_path;
+            article.append(img);
+            populares.append(article);
+        });
+        // Catalogo dos
+        const estrenos = document.getElementById('estreno');
+        catalogoDos.results.forEach(pelicula => {
+            const article = document.createElement('article');
+            article.classList.add('pelicula');
+            const img = document.createElement('img');
+            img.src = 'https://image.tmdb.org/t/p/original/'+pelicula.poster_path;
+            article.append(img);
+            estrenos.append(article);
+        });
+        // Catalogo tres
+        const vistas = document.getElementById('vistas');
+        catalogoTres.results.forEach(pelicula => {
+            const article = document.createElement('article');
+            article.classList.add('pelicula');
+            const img = document.createElement('img');
+            img.src = 'https://image.tmdb.org/t/p/original/'+pelicula.poster_path;
+            article.append(img);
+            vistas.append(article);
+        });
+    });
+});
